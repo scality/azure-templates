c3eaf25e3ad619064d34b178d737277b89f1f4ce
JianqinWang <jianqin.wang@scality.com>
2017-12-04 17:03:02 -0800

    open port 8005 for health checking

--- enterprise/startup-script/run.sh
+++ enterprise/startup-script/run.sh
@@ -5,7 +5,7 @@ export STORAGE_ACCESS_KEY=$2
 export FQDN=$3
 
 sudo nohup docker run -d --name scalityconnect -p 8000:8000 \
--e FQDN=$FQDN \
+-p 8005:8005 -e FQDN=$FQDN \
 -e AZURE_STORAGE_ACCESS_KEY=$STORAGE_ACCESS_KEY \
 -e AZURE_STORAGE_ACCOUNT=$STORAGE_ACCESS_ACCOUNT \
 --log-opt max-size=5g --log-opt max-file=10 \
